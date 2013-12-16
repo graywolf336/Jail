913c5d014ea476ac475040390aa69c31ae2b31de
graywolf336 <graywolf336@craftyn.com>
2013-12-16 14:21:32 -0600

    Meh, start a little work on the jailing command.

--- src/main/java/com/graywolf336/jail/command/commands/JailCommand.java
+++ src/main/java/com/graywolf336/jail/command/commands/JailCommand.java
@@ -1,6 +1,7 @@
 package com.graywolf336.jail.command.commands;
 
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import com.graywolf336.jail.JailManager;
 import com.graywolf336.jail.command.Command;
@@ -12,12 +13,21 @@ import com.graywolf336.jail.command.CommandInfo;
 		needsPlayer = false,
 		pattern = "jail|j",
 		permission = "jail.command.jail",
-		usage = "/jail [name] (time) (j:Jail name) (c:Cell name) (r:Reason) (m:Muted)"
+		usage = "/jail [p:name] (t:time) (j:Jail name) (c:Cell name) (r:Reason) (m:Muted)"
 	)
 public class JailCommand implements Command {
 
 	public boolean execute(JailManager jm, CommandSender sender, String... args) {
-		return true; //If they made it this far, the command is intact and ready to be processed. :)
+		Player p = jm.getPlugin().getServer().getPlayer(args[0]);
+		
+		//Player is not online
+		if(p == null) {
+			sender.sendMessage(args[0] + " is offline.");
+		}else {
+			//Player *is* online
+			sender.sendMessage(args[0] + " is online.");
+		}
+		
+		return true;
 	}
-
 }
