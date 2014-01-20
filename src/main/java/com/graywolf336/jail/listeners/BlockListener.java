28953682aeade85d3a1abafd9f0caac5b32523f8
graywolf336 <graywolf336@craftyn.com>
2014-01-20 12:10:12 -0600

    Add some debugging messages to the placing/breaking so we can see what block are what, will be helpful for figuring out what to add to the whitelist of placing/breaking.

--- src/main/java/com/graywolf336/jail/listeners/BlockListener.java
+++ src/main/java/com/graywolf336/jail/listeners/BlockListener.java
@@ -17,6 +17,9 @@ public class BlockListener implements Listener {
 	
 	@EventHandler(ignoreCancelled=true)
 	public void blockBreak(BlockBreakEvent event) {
+		//If we're in debugging mode, let's send the player what block they're breaking.
+		if(pl.inDebug()) event.getPlayer().sendMessage("[Jail Debug]: " + event.getBlock().getType().toString());
+				
 		//If we are protecting against block breaking, then let's do the action.
 		//If we're not, let's not use any processing power to get the jail
 		//where this block was broke at
@@ -35,6 +38,8 @@ public class BlockListener implements Listener {
 	
 	@EventHandler(ignoreCancelled=true)
 	public void blockPlace(BlockPlaceEvent event) {
+		//If we're in debugging mode, let's send the player what block they're placing.
+		if(pl.inDebug()) event.getPlayer().sendMessage("[Jail Debug]: " + event.getBlock().getType().toString());
 		
 		//If we are protecting against block placing, then let's do the action.
 		//If we're not, let's not use any processing power to get the jail
