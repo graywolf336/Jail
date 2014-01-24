cb8de04a9107887f831309e8dd03f3207d504c01
graywolf336 <graywolf336@craftyn.com>
2014-01-23 18:51:22 -0600

    Temp work on the jailcheck command, not finishing.
    It has been decided we are going to be switching the command structure
    up, more details to come.

--- src/main/java/com/graywolf336/jail/command/commands/JailCheckCommand.java
+++ src/main/java/com/graywolf336/jail/command/commands/JailCheckCommand.java
@@ -5,6 +5,7 @@ import org.bukkit.command.CommandSender;
 import com.graywolf336.jail.JailManager;
 import com.graywolf336.jail.command.Command;
 import com.graywolf336.jail.command.CommandInfo;
+import com.graywolf336.jail.enums.LangString;
 
 @CommandInfo(
 		maxArgs = 1,
@@ -19,7 +20,19 @@ public class JailCheckCommand implements Command{
 	// Checks the status of the specified prisoner, if no args, will display all players currently jailed
 	
 	public boolean execute(JailManager jm, CommandSender sender, String... args) {
-		return true; //If they made it this far, the command is intact and ready to be processed. :)
+		//No args means show everyone
+		if(args.length == 0) {
+			
+		}else {
+			//Otherwise let's check the first arg
+			if(jm.isPlayerJailed(args[0])) {
+				
+			}else {
+				sender.sendMessage(jm.getPlugin().getJailIO().getLanguageString(LangString.NOTJAILED, args[0]));
+			}
+		}
+		
+		return true;
 	}
 
 }
