bfebd6e944f81cb94ff4d3dba84c9fb20a53865d
graywolf336 <graywolf336@craftyn.com>
2014-01-30 14:29:26 -0600

    Fix some of the commands not working, let's hope this doesn't mess up.

--- src/main/java/com/graywolf336/jail/command/subcommands/JailMuteCommand.java
+++ src/main/java/com/graywolf336/jail/command/subcommands/JailMuteCommand.java
@@ -18,19 +18,19 @@ import com.graywolf336.jail.enums.LangString;
 public class JailMuteCommand implements Command {
 	public boolean execute(JailManager jm, CommandSender sender, String... args) throws Exception {
 		//Let's check if the player they're sending us is jailed
-		if(jm.isPlayerJailed(args[0])) {
+		if(jm.isPlayerJailed(args[1])) {
 			//They are, so let's toggle whether they are muted or not
-			boolean muted = !jm.getPrisoner(args[0]).isMuted();
-			jm.getPrisoner(args[0]).setMuted(muted);
+			boolean muted = !jm.getPrisoner(args[1]).isMuted();
+			jm.getPrisoner(args[1]).setMuted(muted);
 			
 			//Send the message to the sender based upon whether they are muted or unmuted
 			if(muted)
-				sender.sendMessage(jm.getPlugin().getJailIO().getLanguageString(LangString.NOWMUTED, args[0]));
+				sender.sendMessage(jm.getPlugin().getJailIO().getLanguageString(LangString.NOWMUTED, args[1]));
 			else
-				sender.sendMessage(jm.getPlugin().getJailIO().getLanguageString(LangString.NOWUNMUTED, args[0]));
+				sender.sendMessage(jm.getPlugin().getJailIO().getLanguageString(LangString.NOWUNMUTED, args[1]));
 		}else {
 			//The player provided is not jailed, so let's tell the sender that
-			sender.sendMessage(jm.getPlugin().getJailIO().getLanguageString(LangString.NOTJAILED, args[0]));
+			sender.sendMessage(jm.getPlugin().getJailIO().getLanguageString(LangString.NOTJAILED, args[1]));
 		}
 		
 		return true;
--- src/main/java/com/graywolf336/jail/command/subcommands/JailTeleOutCommand.java
+++ src/main/java/com/graywolf336/jail/command/subcommands/JailTeleOutCommand.java
@@ -19,30 +19,30 @@ import com.graywolf336.jail.enums.LangString;
 	)
 public class JailTeleOutCommand implements Command {
 	public boolean execute(JailManager jm, CommandSender sender, String... args) throws Exception {
-		Jail j = jm.getJail(args[0]);
+		Jail j = jm.getJail(args[1]);
 		
 		//The jail doesn't exist
 		if(j == null) {
-			sender.sendMessage(jm.getPlugin().getJailIO().getLanguageString(LangString.NOJAIL, args[0]));
+			sender.sendMessage(jm.getPlugin().getJailIO().getLanguageString(LangString.NOJAIL, args[1]));
 		}else {
 			//The jail does exist
 			//now let's check the size of the command
 			//if it has two args then someone is sending someone else in
 			//otherwise it is just the sender going in
-			if(args.length == 2) {
-				Player p = jm.getPlugin().getServer().getPlayer(args[1]);
+			if(args.length == 3) {
+				Player p = jm.getPlugin().getServer().getPlayer(args[2]);
 				
 				//If the player they're trying to send is offline, don't do anything
 				if(p == null) {
-					sender.sendMessage(jm.getPlugin().getJailIO().getLanguageString(LangString.PLAYERNOTONLINE, args[1]));
+					sender.sendMessage(jm.getPlugin().getJailIO().getLanguageString(LangString.PLAYERNOTONLINE, args[2]));
 				}else {
 					p.teleport(j.getTeleportFree());
-					sender.sendMessage(jm.getPlugin().getJailIO().getLanguageString(LangString.TELEOUT, new String[] { args[1], args[0] }));
+					sender.sendMessage(jm.getPlugin().getJailIO().getLanguageString(LangString.TELEOUT, new String[] { args[2], args[1] }));
 				}
 			}else {
 				if(sender instanceof Player) {
 					((Player) sender).teleport(j.getTeleportFree());
-					sender.sendMessage(jm.getPlugin().getJailIO().getLanguageString(LangString.TELEOUT, new String[] { sender.getName(), args[0] }));
+					sender.sendMessage(jm.getPlugin().getJailIO().getLanguageString(LangString.TELEOUT, new String[] { sender.getName(), args[1] }));
 				}else {
 					sender.sendMessage(jm.getPlugin().getJailIO().getLanguageString(LangString.PLAYERCONTEXTREQUIRED));
 				}
