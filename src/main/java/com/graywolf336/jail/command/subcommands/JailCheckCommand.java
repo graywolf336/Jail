b1ca15d10c2ae7dc24978a23667b24c13a850371
graywolf336 <graywolf336@craftyn.com>
2014-02-12 15:35:29 -0600

    Jail check is now for one player only, use jail list <jail> for all.

--- src/main/java/com/graywolf336/jail/command/subcommands/JailCheckCommand.java
+++ src/main/java/com/graywolf336/jail/command/subcommands/JailCheckCommand.java
@@ -1,34 +1,35 @@
 package com.graywolf336.jail.command.subcommands;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 
 import com.graywolf336.jail.JailManager;
+import com.graywolf336.jail.beans.Prisoner;
 import com.graywolf336.jail.command.Command;
 import com.graywolf336.jail.command.CommandInfo;
 import com.graywolf336.jail.enums.LangString;
 
 @CommandInfo(
 		maxArgs = 1,
-		minimumArgs = 0,
+		minimumArgs = 1,
 		needsPlayer = false,
 		pattern = "check",
 		permission = "jail.command.jailcheck",
-		usage = "/jail check (Player name)"
+		usage = "/jail check <playername>"
 	)
 public class JailCheckCommand implements Command{
 
-	// Checks the status of the specified prisoner, if no args, will display all players currently jailed
+	// Checks the status of the specified prisoner
 	public boolean execute(JailManager jm, CommandSender sender, String... args) {
-		//No args means show everyone
-		if(args.length == 1) {
-			//TODO
+		//Otherwise let's check the first argument
+		if(jm.isPlayerJailed(args[1])) {
+			Prisoner p = jm.getPrisoner(args[1]);
+			
+			//graywolf663: Being gray's evil twin; CONSOLE (10)
+			//prisoner: reason; jailer (time in minutes)
+			sender.sendMessage(ChatColor.BLUE + " " + p.getName() + ": " + p.getReason() + "; " + p.getJailer() + " (" + p.getRemainingTimeInMinutes() + " mins)");
 		}else {
-			//Otherwise let's check the first arg
-			if(jm.isPlayerJailed(args[1])) {
-				//TODO
-			}else {
-				sender.sendMessage(jm.getPlugin().getJailIO().getLanguageString(LangString.NOTJAILED, args[1]));
-			}
+			sender.sendMessage(jm.getPlugin().getJailIO().getLanguageString(LangString.NOTJAILED, args[1]));
 		}
 		
 		return true;
