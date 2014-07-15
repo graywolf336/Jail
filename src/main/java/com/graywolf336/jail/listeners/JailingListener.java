8fe7dea3aea4103165da0281adb99ba9b6af0cb0
graywolf336 <graywolf336@craftyn.com>
2014-07-14 19:05:28 -0500

    Add limited inmate clothing! Closes Ticket #22

--- src/main/java/com/graywolf336/jail/listeners/JailingListener.java
+++ src/main/java/com/graywolf336/jail/listeners/JailingListener.java
@@ -4,13 +4,19 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.bukkit.Color;
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.LeatherArmorMeta;
 
 import com.graywolf336.jail.JailMain;
 import com.graywolf336.jail.enums.LangString;
+import com.graywolf336.jail.enums.Settings;
 import com.graywolf336.jail.events.PrePrisonerJailedByJailStickEvent;
 import com.graywolf336.jail.events.PrePrisonerJailedEvent;
+import com.graywolf336.jail.events.PrisonerJailedEvent;
 
 public class JailingListener implements Listener {
 	private JailMain pl;
@@ -35,5 +41,74 @@ public class JailingListener implements Listener {
 				event.getPrisoner().getLastKnownName(),
 				event.getPrisoner().getJailer(), dateFormat.format(new Date()),
 				event.getPrisoner().getRemainingTimeInMinutes(), event.getPrisoner().getReason());
+	}
+	
+	@EventHandler
+	public void setInmatesClothing(PrisonerJailedEvent event) {
+		if(pl.getConfig().getBoolean(Settings.CLOTHINGENABLED.getPath())) {
+			String[] helmet = pl.getConfig().getString(Settings.CLOTHINGHELMET.getPath()).split("~");
+			switch(helmet.length) {
+				case 1:
+					event.getPlayer().getInventory().setHelmet(new ItemStack(Material.valueOf(helmet[0])));
+					break;
+				case 2:
+					ItemStack item = new ItemStack(Material.valueOf(helmet[0]));
+					LeatherArmorMeta meta = (LeatherArmorMeta) item.getItemMeta();
+					String[] colors = helmet[1].split(",");
+					meta.setColor(Color.fromBGR(Integer.parseInt(colors[0]), Integer.parseInt(colors[1]), Integer.parseInt(colors[2])));
+					item.setItemMeta(meta);
+					event.getPlayer().getInventory().setHelmet(item);
+				default:
+					break;
+			}
+			
+			String[] chest = pl.getConfig().getString(Settings.CLOTHINGCHEST.getPath()).split("~");
+			switch(chest.length) {
+				case 1:
+					event.getPlayer().getInventory().setChestplate(new ItemStack(Material.valueOf(chest[0])));
+					break;
+				case 2:
+					ItemStack item = new ItemStack(Material.valueOf(chest[0]));
+					LeatherArmorMeta meta = (LeatherArmorMeta) item.getItemMeta();
+					String[] colors = chest[1].split(",");
+					meta.setColor(Color.fromBGR(Integer.parseInt(colors[0]), Integer.parseInt(colors[1]), Integer.parseInt(colors[2])));
+					item.setItemMeta(meta);
+					event.getPlayer().getInventory().setChestplate(item);
+				default:
+					break;
+			}
+			
+			String[] legs = pl.getConfig().getString(Settings.CLOTHINGLEGS.getPath()).split("~");
+			switch(legs.length) {
+				case 1:
+					event.getPlayer().getInventory().setLeggings(new ItemStack(Material.valueOf(legs[0])));
+					break;
+				case 2:
+					ItemStack item = new ItemStack(Material.valueOf(legs[0]));
+					LeatherArmorMeta meta = (LeatherArmorMeta) item.getItemMeta();
+					String[] colors = legs[1].split(",");
+					meta.setColor(Color.fromBGR(Integer.parseInt(colors[0]), Integer.parseInt(colors[1]), Integer.parseInt(colors[2])));
+					item.setItemMeta(meta);
+					event.getPlayer().getInventory().setLeggings(item);
+				default:
+					break;
+			}
+			
+			String[] boots = pl.getConfig().getString(Settings.CLOTHINGBOOTS.getPath()).split("~");
+			switch(boots.length) {
+				case 1:
+					event.getPlayer().getInventory().setBoots(new ItemStack(Material.valueOf(boots[0])));
+					break;
+				case 2:
+					ItemStack item = new ItemStack(Material.valueOf(boots[0]));
+					LeatherArmorMeta meta = (LeatherArmorMeta) item.getItemMeta();
+					String[] colors = boots[1].split(",");
+					meta.setColor(Color.fromBGR(Integer.parseInt(colors[0]), Integer.parseInt(colors[1]), Integer.parseInt(colors[2])));
+					item.setItemMeta(meta);
+					event.getPlayer().getInventory().setBoots(item);
+				default:
+					break;
+			}
+		}
 	}
 }
